436d22ea219ad641780d91e4436ad6d3cc201f69
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-06 12:49:50 -0500

    did code and switched comments up

--- main.ts
+++ main.ts
@@ -5,53 +5,77 @@
  * This program turns on different colored pins. 
 */
 
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+pause(500)
+pins.digitalWritePin(DigitalPin.P13, 0) // the blue
+pins.digitalWritePin(DigitalPin.P14, 0) // the green
+pins.digitalWritePin(DigitalPin.P15, 0) // the red
 
+
+
+// the program
 input.onButtonPressed(Button.A, function () {
-    // turns on red pin
     basic.showIcon(IconNames.Yes)
-    pins.digitalWritePin(DigitalPin.P14, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P14, 0)
+    pause(1000)
+    basic.clearScreen()
 
-    // turns on green pin
+    // shows the red
     pins.digitalWritePin(DigitalPin.P15, 1)
-    basic.pause(1000)
+    basic.showString("Red")
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
 
-    // turns on blue pin
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-
-    // turns on blue and red pin
+    // shows the green
     pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(1000)
+    basic.showString("Green")
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
 
-    // turns on green and red pin
+    // show blue
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Blue")
+    pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+    //shows the magenta
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Magenta")
+    pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+    // shows the cyan
     pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Cyan")
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 
-    // turns on green and blue pin aka megenta
+    // shows the yellow
     pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-
-    // turns on green, red and blue pin
     pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.showString("Yellow")
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    //shows the white
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("White")
+    pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+
+
+    //resets it
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
 })
